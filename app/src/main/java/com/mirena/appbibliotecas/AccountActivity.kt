f6435bdf66cfb14730d115f3ad524d88c8b3e015
misge1 <misge@alumni.uv.es>
2022-12-15 20:11:49 +0100

    muchas cosas

--- app/src/main/java/com/mirena/appbibliotecas/AccountActivity.kt
+++ app/src/main/java/com/mirena/appbibliotecas/AccountActivity.kt
@@ -3,7 +3,7 @@ package com.mirena.appbibliotecas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
